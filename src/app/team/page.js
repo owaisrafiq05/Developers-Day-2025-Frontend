b1780434a4fd03ed77f3c4925ca2e96e64963580
Abdullah <mafishere@gmail.com>
2025-02-28 00:10:33 +0500

    added contact us

--- src/app/team/page.js
+++ src/app/team/page.js
@@ -139,7 +139,13 @@ export default function Team() {
     <div>
     <TeamHero />
     <section ref={sectionRef} className="relative overflow-hidden bg-white">
-      <Squares />
+    <Squares
+          speed={0.5}
+          squareSize={40}
+          direction="diagonal" // up, down, left, right, diagonal
+          borderColor="#000"
+          hoverFillColor="#222"
+        />
       
       <div className="container mx-auto px-4 py-16">
         {/* Hero content */}
