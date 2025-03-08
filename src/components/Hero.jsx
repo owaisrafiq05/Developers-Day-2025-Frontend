1c3116ae9c66c4bc8c7a93a03331ce551e7748ee
owaisrafiq05 <owaisrafiq05@gmail.com>
2025-03-08 16:49:20 +0500

    Bug Fix 01

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,10 @@
 "use client";
 
-
 import Countdown from "./Counter";
-
-
-
 import Experience from "./Experience";
 import { Canvas } from "@react-three/fiber";
 import PageHeader from "./3js/Home";
 import Squares from "@/components/Squares/Squares";
-
 
 const Hero = () => {
   return (
@@ -23,20 +18,20 @@ const Hero = () => {
         className="fixed top-0 left-0 w-screen h-screen z-0"
       />
       
-      <Canvas
-        className="fixed top-0 left-0 canvas z-10 pt-20"
-        shadows
-        camera={{
-          fov: 45,
-          near: 0.1,
-          far: 1000,
-          position: [27.216, -0.060, 5.026],
-        }}
-      >
-        <Experience />
-      </Canvas>
+      <div className="relative min-h-full z-20 grid grid-cols-1">
+        <Canvas
+          className="canvas z-10 pt-20 mb-10"
+          shadows
+          camera={{
+            fov: 45,
+            near: 0.1,
+            far: 1000,
+            position: [27.216, -0.060, 5.026],
+          }}
+        >
+          <Experience />
+        </Canvas>
 
-      <div className="relative min-h-full z-20">
         <div className="z-20">
           <PageHeader />
         </div>
@@ -44,5 +39,6 @@ const Hero = () => {
     </> 
   );
 }
+
 export default Hero;
 
