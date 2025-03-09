b2990d1d7c2d5d5095cb3bacc55f16deb17a6ed4
Ahad Aziz Jaffer <k213241@nu.edu.pk>
2025-03-09 05:34:08 +0500

    prize pool on fyp

--- src/app/project-xtreme/page.js
+++ src/app/project-xtreme/page.js
@@ -134,6 +134,25 @@ const ProjectXtreme = () => {
          </div>
        </div> */}
 
+       <motion.div
+          className="w-full py-6 my-10"
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          variants={fadeInOut}
+          viewport={{ once: false, amount: 0.2 }}
+        >
+          <div className="container mx-auto flex items-center justify-center px-4">
+            <div className="flex items-center gap-2 px-6 py-3 bg-black/40 backdrop-blur-sm rounded-full border border-red-600/50 shadow-md">
+              <FaTrophy className="text-white text-2xl md:text-3xl mr-1" />
+              <span className="text-xl md:text-2xl font-bold text-white mr-1">Prize pool:</span>
+              <span className="text-2xl md:text-4xl font-extrabold text-white">
+                PKR. 1 Lac
+              </span>
+            </div>
+          </div>
+        </motion.div>
+
 
         <motion.section
           className="container mx-auto space-y-20 py-10 mt-20"
