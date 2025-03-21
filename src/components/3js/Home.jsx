54e5f111ca4ff578a06303266be42147e30c6a47
owaisrafiq05 <owaisrafiq05@gmail.com>
2025-03-21 18:29:31 +0500

    Link Fixed

--- src/components/3js/Home.jsx
+++ src/components/3js/Home.jsx
@@ -1,9 +1,7 @@
+import Link from "next/link";
 import HorizontalList from "./widgets/HorizontalList";
 import Light from "./widgets/atomic/Light";
 import GradientTag from "./widgets/atomic/GradientTag";
-
-
-
 
 const horizontalUserStats = [
   {
@@ -16,9 +14,6 @@ const horizontalUserStats = [
     text: "Active Orders : +51",
   },
 ];
-
-
-
 
 const PageHeader = () => {
   return (
@@ -46,16 +41,15 @@ const PageHeader = () => {
               [text-shadow:_0_0_30px_rgb(255_51, 51, 40%)]">
               2025
             </span>
-            
           </div>
           <div className="ml-10">
             <span className="text-xl lg-text-3xl md:ml-2">17th April 2025 at FAST NUCES, Karachi</span>
           </div>
 
           <div className="ml-10 mt-8">
-            <a 
-              href="/registration"
-              className="relative inline-flex items-center justify-center px-8 py-4 
+            <Link href="/registration">
+              <span 
+                className="relative inline-flex items-center justify-center px-8 py-4 
                 overflow-hidden font-bold rounded-lg group bg-gradient-to-br 
                 from-[#ff3333] to-[#ff6666] text-white
                 transition-all duration-300 ease-out
@@ -69,32 +63,31 @@ const PageHeader = () => {
                 after:bg-gradient-to-br after:from-[#ff3333] after:to-[#ff6666]
                 after:blur-xl after:transition-all after:duration-300
                 after:scale-105 after:opacity-40
-                hover:after:scale-110 hover:after:opacity-70"
-            >
-              <span className="relative z-10 flex items-center">
-                Register Now
-                <svg 
-                  className="ml-2 -mr-1 w-4 h-4" 
-                  fill="currentColor" 
-                  viewBox="0 0 20 20" 
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path 
-                    fillRule="evenodd" 
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" 
-                    clipRule="evenodd"
-                  />
-                </svg>
+                hover:after:scale-110 hover:after:opacity-70
+                cursor-pointer"
+              >
+                <span className="relative z-10 flex items-center">
+                  Register Now
+                  <svg 
+                    className="ml-2 -mr-1 w-4 h-4" 
+                    fill="currentColor" 
+                    viewBox="0 0 20 20" 
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path 
+                      fillRule="evenodd" 
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" 
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
               </span>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
-
-     
     </>
   );
 };
-
 
 export default PageHeader;
